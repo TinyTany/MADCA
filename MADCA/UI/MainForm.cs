0f808ed70d90ed93ef7a24bc1c8a75a1806be9eb
TinyTany <t4nishi.x@gmail.com>
2019-11-05 18:27:57 +0900

    メインフォームの名前と階層変更

---
+++ MADCA/UI/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MADCA.UI
+{
+    public partial class MainForm : Form
+    {
+        private readonly string appName = "MADCA";
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = appName;
+        }
+    }
+}
