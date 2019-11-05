380212c3d0d6b23a2c00f87015d0c2ebe6cbddd1
TinyTany <t4nishi.x@gmail.com>
2019-11-06 02:15:23 +0900

    ノーツ用のインターフェースを少し用意

---
+++ MADCA/Core/Note/ILongNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Note
+{
+    public interface ILongNote
+    {
+        List<IStepNote> Notes { get; }
+
+        bool Put(IStepNote note);
+
+        bool UnPut(IStepNote note);
+    }
+}
---
+++ MADCA/Core/Note/INote.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MADCA.Core.Data;
+
+namespace MADCA.Core.Note
+{
+    public enum NoteType
+    {
+        Unknown,
+        Touch,
+        Chain, 
+        SlideL, 
+        SlideR, 
+        SnapU, 
+        SnapD, 
+        HoldBegin,
+        HoldRelay,
+        HoldEnd
+    }
+    public interface INote
+    {
+        NoteType NoteType { get; set; }
+        LanePotision Lane { get; set; }
+        TimingPosition Timing { get; set; }
+        NoteSize NoteSize { get; set; }
+    }
+}
