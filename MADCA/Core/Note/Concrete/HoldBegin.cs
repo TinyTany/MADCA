87cede2b6bbaf4ed331cd4fc366bdd0e86ad0df5
TinyTany <t4nishi.x@gmail.com>
2019-11-25 22:30:33 +0900

    HoldStep系ノーツクラスのコンストラクタ追加

--- MADCA/Core/Note/Concrete/HoldBegin.cs
+++ MADCA/Core/Note/Concrete/HoldBegin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MADCA.Core.Data;
 using MADCA.Core.Note.Abstract;
 using MADCA.Core.Note.Interface;
 
@@ -11,5 +12,10 @@ namespace MADCA.Core.Note.Concrete
     public sealed class HoldBegin : HoldStepNote
     {
         public override NoteType NoteType => NoteType.HoldBegin;
+
+        private HoldBegin() { }
+
+        public HoldBegin(LanePotision lane, TimingPosition timing, NoteSize size)
+            : base(lane, timing, size) { }
     }
 }
