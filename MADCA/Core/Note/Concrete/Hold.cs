8b2f3cf4a4e49eb2fde24d60d8f681a937f37dbb
TinyTany <t4nishi.x@gmail.com>
2019-11-25 22:32:24 +0900

    Holdの実装
    Step系Noteの追加削除メソッドなど

--- MADCA/Core/Note/Concrete/Hold.cs
+++ MADCA/Core/Note/Concrete/Hold.cs
@@ -9,48 +9,70 @@ using MADCA.Core.Data;
 
 namespace MADCA.Core.Note.Concrete
 {
-    public sealed class Hold// : ILongNote
+    public sealed class Hold : ILongNote<HoldStepNote>
     {
         private readonly List<HoldStepNote> stepNotes;
+        private readonly HoldBegin holdBegin;
 
-        public Hold()
+        private Hold()
         {
+            holdBegin = null;
             stepNotes = new List<HoldStepNote>();
         }
 
-        public bool Put(in HoldStepNote note)
+        public Hold(LanePotision lane, TimingPosition timingBegin, TimingPosition timingEnd, NoteSize size)
         {
+            System.Diagnostics.Debug.Assert(timingBegin < timingEnd);
+            holdBegin = new HoldBegin(lane, timingBegin, size);
+            holdBegin.PositionChanging += (begin, beginLane, beginTiming) =>
+            {
+                if (stepNotes.Where(x => x.Timing <= beginTiming).Any())
+                {
+                    return false;
+                }
+                return true;
+            };
+            stepNotes = new List<HoldStepNote>();
+            var result = Put(new HoldRelay(lane, timingEnd, size));
+            System.Diagnostics.Debug.Assert(result);
+        }
+
+        public bool Put(HoldStepNote note)
+        {
+            if (note is null) { return false; }
+            if (!IsStepNoteTimingValid(note.Timing)) { return false; }
             stepNotes.Add(note);
             note.PositionChanging += (step, lane, timing) =>
             {
                 switch (step.NoteType)
                 {
-                    case NoteType.HoldBegin:
-                        {
-
-                        }
-                        break;
                     case NoteType.HoldRelay:
                         {
-
+                            return IsStepNoteTimingValid(timing);
                         }
-                        break;
-                    case NoteType.HoldEnd:
-                        {
-
-                        }
-                        break;
                     default:
                         return false;
                 }
-                return true;
             };
             return true;
         }
 
-        public bool UnPut(in HoldStepNote note)
+        public bool UnPut(HoldStepNote note)
         {
-            throw new NotImplementedException();
+            var result = stepNotes.Remove(note);
+            if (result)
+            {
+                note.RemoveAllEvents();
+            }
+            return result;
+        }
+
+        private bool IsStepNoteTimingValid(TimingPosition timing)
+        {
+            if (timing is null) { return false; }
+            if (timing <= holdBegin.Timing) { return false; }
+            if (stepNotes.Where(x => x.Timing == timing).Any()) { return false; }
+            return true;
         }
     }
 }
