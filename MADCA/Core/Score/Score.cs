7d9519c856030dbf0c09828a6eb4c713ed4de396
TinyTany <t4nishi.x@gmail.com>
2019-12-01 14:25:46 +0900

    Scoreクラス実装
    純粋なデータとしてはこれくらいシンプルか

---
+++ MADCA/Core/Score/Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Score
+{
+    public sealed class Score
+    {
+        public int BeatNum { get; private set; }
+        public int BeatDen { get; private set; }
+
+        private Score() { }
+
+        public Score(int num, int den)
+        {
+            BeatNum = num;
+            BeatDen = den;
+            if (BeatNum <= 0 || BeatDen <= 0)
+            {
+                // NOTE: 4/4拍子をデフォルト値として決め打ち
+                BeatNum = BeatDen = 4;
+            }
+        }
+    }
+}
