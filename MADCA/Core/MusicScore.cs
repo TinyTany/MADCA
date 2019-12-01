7fca8347d13777d3b9630a233a634edc3aeabb97
TinyTany <t4nishi.x@gmail.com>
2019-12-02 02:52:50 +0900

    MusicScoreクラス追加
    ノーツと小節のデータを合わせた譜面全体を表すクラス

---
+++ MADCA/Core/MusicScore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MADCA.Core.Note;
+using MADCA.Core.Score;
+
+namespace MADCA.Core
+{
+    public sealed class MusicScore
+    {
+        private readonly ScoreBook scoreBook;
+        private readonly NoteBook noteBook;
+
+        public MusicScore()
+        {
+            scoreBook = new ScoreBook();
+            noteBook = new NoteBook();
+        }
+
+        // TODO: NOTE: ノーツや小節の操作はScoreBook、NoteBookを触るのではなく、
+        //             このクラスを介してやるようにしたいのでそういう実装をする
+    }
+}
