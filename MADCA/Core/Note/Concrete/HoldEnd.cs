3b2978e9528fcafef9f6cc1d2fbf19145e9b4ea6
TinyTany <t4nishi.x@gmail.com>
2019-12-01 18:20:09 +0900

    HoldEndを調整
    復活させたHoldEndクラスを調整

--- MADCA/Core/Note/Concrete/HoldEnd.cs
+++ MADCA/Core/Note/Concrete/HoldEnd.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MADCA.Core.Data;
 using MADCA.Core.Note.Abstract;
 using MADCA.Core.Note.Interface;
 
@@ -11,5 +12,10 @@ namespace MADCA.Core.Note.Concrete
     public sealed class HoldEnd : HoldStepNote
     {
         public override NoteType NoteType => NoteType.HoldEnd;
+
+        private HoldEnd() { }
+
+        public HoldEnd(LanePotision lane, TimingPosition timing, NoteSize size)
+            : base(lane, timing, size) { }
     }
 }
