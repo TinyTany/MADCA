547641fe0a63abda3346cfaf4c1409ffa87016bd
TinyTany <t4nishi.x@gmail.com>
2019-12-02 02:50:16 +0900

    クラス名変更と実装追加
    GameDisplayBoxは描画をするだけのクラス（ゲームプレイ画面プレビュー用）

---
+++ MADCA/UI/GameDisplayBox.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MADCA.UI
+{
+    public sealed class GameDisplayBox
+    {
+        public event Action<PaintEventArgs> Paint;
+        private readonly PictureBox pictureBox;
+
+        private GameDisplayBox() { }
+
+        public GameDisplayBox(PictureBox box)
+        {
+            pictureBox = box;
+            pictureBox.Paint += (s, e) => Paint?.Invoke(e);
+        }
+
+        public void Refresh() => pictureBox.Refresh();
+    }
+}
