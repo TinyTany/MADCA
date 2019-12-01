ce782febffebed24041b0f5c117c700582c8811c
TinyTany <t4nishi.x@gmail.com>
2019-12-01 18:21:12 +0900

    ShortNoteクラス追加
    ノーツ具象クラスの分類のため追加

---
+++ MADCA/Core/Note/Abstract/ShortNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MADCA.Core.Data;
+
+namespace MADCA.Core.Note.Abstract
+{
+    public abstract class ShortNote : Note
+    {
+        protected ShortNote() { }
+
+        protected ShortNote(LanePotision lane, TimingPosition timing, NoteSize size)
+            : base(lane, timing, size) { }
+    }
+}
