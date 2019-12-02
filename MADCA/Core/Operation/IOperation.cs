5f33a2b6ba59f32f4e0308e07f70f4f0e272567c
TinyTany <t4nishi.x@gmail.com>
2019-12-03 02:19:53 +0900

    操作を扱うクラス追加

---
+++ MADCA/Core/Operation/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Operation
+{
+    public interface IOperation
+    {
+        void Invoke();
+        void Undo();
+    }
+}
---
+++ MADCA/Core/Operation/OperationManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Operation
+{
+    public class OperationManager
+    {
+        private readonly Stack<IOperation> stackUndo, stackRedo;
+
+        public OperationManager()
+        {
+            stackUndo = new Stack<IOperation>();
+            stackRedo = new Stack<IOperation>();
+        }
+
+        public void AddOperation(IOperation op)
+        {
+            stackUndo.Push(op);
+            stackRedo.Clear();
+        }
+
+        public void AddAndInvokeOperation(IOperation op)
+        {
+            AddOperation(op);
+            op.Invoke();
+        }
+
+        public void Undo()
+        {
+            if (stackUndo.Any())
+            {
+                var op = stackUndo.Pop();
+                op.Undo();
+                stackRedo.Push(op);
+            }
+        }
+
+        public void Redo()
+        {
+            if (stackRedo.Any())
+            {
+                var op = stackRedo.Pop();
+                op.Invoke();
+                stackUndo.Push(op);
+            }
+        }
+    }
+}
