7e8c07be6769ff163d560a4da957f57f53603886
TinyTany <t4nishi.x@gmail.com>
2019-12-03 02:24:31 +0900

    MaterialBookクラス追加

---
+++ MADCA/Core/MaterialBox/MaterialBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.MaterialBox
+{
+    public sealed class MaterialBook
+    {
+        private readonly List<NoteBox> notes;
+        private readonly List<ScoreBox> scores;
+
+        public MaterialBook()
+        {
+            notes = new List<NoteBox>();
+            scores = new List<ScoreBox>();
+        }
+    }
+}
