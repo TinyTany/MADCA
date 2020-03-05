9ca98d0d72c541576f99ed5be3645544c5ae62e2
TinyTany <t4nishi.x@gmail.com>
2020-03-06 01:58:13 +0900

    Environmentクラス追加
    マウス座標から譜面位置を出すときとかに、実際のレーンの構造の諸々の値とかを知る必要があるのでそれをまとめて渡せるようにするためのクラス？
    要検討

---
+++ MADCA/Core/Data/Environment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Data
+{
+    public sealed class EditorLaneEnvironment
+    {
+        public static readonly int LaneCount = 60;
+        public float LeftMargin { get; private set; }
+        public float RightMargin { get; private set; }
+        public float LaneUnitWidth { get; private set; } = 10f;
+        public float TimingUnitHeight { get; private set; }
+    }
+}
