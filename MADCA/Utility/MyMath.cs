13d48da2531ca99ff4663295d9421de77a2cb8de
TinyTany <t4nishi.x@gmail.com>
2020-03-06 01:58:59 +0900

    MyMathクラス追加
    よく使いそうな計算用の関数とかはここに

---
+++ MADCA/Utility/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Utility
+{
+    public static class MyMath
+    {
+        public static uint Gcd(uint a, uint b)
+        {
+            if (b == 0) { return a; }
+            return Gcd(b, a % b);
+        }
+
+        public static int Lcm(int a, int b)
+        {
+            return (int)(a / Gcd((uint)a, (uint)b) * b);
+        }
+    }
+}
