868756f175843f617d397db03a449c227772b81e
TinyTany <t4nishi.x@gmail.com>
2020-03-06 01:59:36 +0900

    PositionConverterクラス追加
    マウス座標から譜面座標を出すための関数とかを実装する
    要実装

---
+++ MADCA/Utility/PositionConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MADCA.Core.Data;
+using System.Drawing;
+
+namespace MADCA.Utility
+{
+    public static class PositionConverter
+    {
+        public static (LanePotision, TimingPosition) ConvertUniversePosition(Point p, float t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
