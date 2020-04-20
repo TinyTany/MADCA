27465f5d1ec0633136322bc3ec9dcddbe95cec7e
TinyTany <t4nishi.x@gmail.com>
2020-04-20 17:15:34 +0900

    Update Environment.cs
    EditorLaneEnvironmentクラスは読み書き可能にしておき、読み取り専用のインターフェースを追加

--- MADCA/Core/Data/Environment.cs
+++ MADCA/Core/Data/Environment.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace MADCA.Core.Data
 {
-    public sealed class EditorLaneEnvironment
+    public interface IReadOnlyEditorLaneEnvironment
     {
-        public static readonly int LaneCount = 60;
-        public float LeftMargin { get; private set; }
-        public float RightMargin { get; private set; }
-        public float LaneUnitWidth { get; private set; } = 10f;
-        public float TimingUnitHeight { get; private set; }
+        uint LaneCount { get; }
+        uint SideMargin { get; }
+        uint LaneUnitWidth { get; }
+        uint TimingUnitHeight { get; }
+        Size EditorLaneSize { get; }
+        uint AvailableLaneWidth { get; }
+    }
+
+    public sealed class EditorLaneEnvironment : IReadOnlyEditorLaneEnvironment
+    {
+        /// <summary>
+        /// レーン数（不変）
+        /// </summary>
+        public uint LaneCount => 60;
+        private readonly uint sideMarginMin = 100;
+        public uint SideMargin
+        {
+            get
+            {
+                var margin = (EditorLaneSize.Width - LaneUnitWidth * LaneCount) / 2;
+                if (margin <= sideMarginMin) { return sideMarginMin; }
+                return (uint)margin;
+            }
+        }
+        /// <summary>
+        /// Lane幅1に対応する画面上でのピクセル数
+        /// </summary>
+        public uint LaneUnitWidth { get; set; } = 10;
+        /// <summary>
+        /// Timing幅1に対応する画面上でのピクセル数
+        /// </summary>
+        public uint TimingUnitHeight { get; set; } = 192;
+        public Size EditorLaneSize { get; set; }
+        public uint AvailableLaneWidth
+        {
+            get
+            {
+                var width = EditorLaneSize.Width - sideMarginMin * 2;
+                if (width <= 0) { return 0; }
+                if (width >= LaneUnitWidth * LaneCount) { return LaneUnitWidth * LaneCount; }
+                return (uint)width;
+            }
+        }
+        
+        public EditorLaneEnvironment(Size size)
+        {
+            EditorLaneSize = size;
+        }
     }
 }
