836d7c27ef9201211bdf654160f40352ff958536
TinyTany <t4nishi.x@gmail.com>
2020-04-22 16:39:05 +0900

    テスト作成

---
+++ MADCATests/Core/Data/TimingPositionTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MADCA.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Core.Data.Tests
+{
+    [TestClass()]
+    public class TimingPositionTests
+    {
+        [TestMethod()]
+        public void TimingPositionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TimingPositionTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
