49135bde5245e114a348d99a564c256fd58f4c78
TinyTany <t4nishi.x@gmail.com>
2020-05-07 02:23:02 +0900

    Update LanePotision.cs
    Positionクラスを追加（LaneとTimingのペア）

--- MADCA/Core/Data/LanePotision.cs
+++ MADCA/Core/Data/LanePotision.cs
@@ -15,6 +15,7 @@ namespace MADCA.Core.Data
             get
             {
                 // NOTE: 本家の仕様としてレーン数は60個
+                // HACK: ハードコーディングしてるけど大丈夫かな
                 if (RawLane >= 0)
                 {
                     return RawLane % 60;
@@ -30,5 +31,22 @@ namespace MADCA.Core.Data
 
         public LanePotision (LanePotision other)
             : this(other.RawLane) { }
+    }
+
+    public class Position
+    {
+        public LanePotision Lane { get; private set; }
+        public TimingPosition Timing { get; private set; }
+        private Position() { }
+        public Position(LanePotision lane, TimingPosition timing)
+        {
+            Lane = lane;
+            Timing = timing;
+        }
+        public Position(Position p)
+        {
+            Lane = new LanePotision(p.Lane);
+            Timing = new TimingPosition(p.Timing);
+        }
     }
 }
