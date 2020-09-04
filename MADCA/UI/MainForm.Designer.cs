95a2faf5c557382dbf66d8c54ddd62b2fe2f9d33
TinyTany <t4nishi.x@gmail.com>
2020-09-05 00:44:41 +0900

    MainFormのUI周り変更
    多分メニューバーを追加したりしたなどに伴う変更

--- MADCA/UI/MainForm.Designer.cs
+++ MADCA/UI/MainForm.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tslTouchNote = new System.Windows.Forms.ToolStripLabel();
+            this.tslChainNote = new System.Windows.Forms.ToolStripLabel();
+            this.tslSlideLeftNote = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1634, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOpen,
+            this.tsmiQuit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F)));
+            this.tsmiFile.Size = new System.Drawing.Size(67, 20);
+            this.tsmiFile.Text = "ファイル(&F)";
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.tsmiOpen.Size = new System.Drawing.Size(161, 22);
+            this.tsmiOpen.Text = "開く(&O)...";
+            // 
+            // tsmiQuit
+            // 
+            this.tsmiQuit.Name = "tsmiQuit";
+            this.tsmiQuit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.tsmiQuit.Size = new System.Drawing.Size(161, 22);
+            this.tsmiQuit.Text = "終了(&Q)";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslTouchNote,
+            this.tslChainNote,
+            this.tslSlideLeftNote});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1634, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tslTouchNote
+            // 
+            this.tslTouchNote.Name = "tslTouchNote";
+            this.tslTouchNote.Size = new System.Drawing.Size(39, 22);
+            this.tslTouchNote.Text = "Touch";
+            // 
+            // tslChainNote
+            // 
+            this.tslChainNote.Name = "tslChainNote";
+            this.tslChainNote.Size = new System.Drawing.Size(37, 22);
+            this.tslChainNote.Text = "Chain";
+            // 
+            // tslSlideLeftNote
+            // 
+            this.tslSlideLeftNote.Name = "tslSlideLeftNote";
+            this.tslSlideLeftNote.Size = new System.Drawing.Size(52, 22);
+            this.tslSlideLeftNote.Text = "SlideLeft";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "";
+            this.ClientSize = new System.Drawing.Size(1634, 861);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiQuit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel tslTouchNote;
+        private System.Windows.Forms.ToolStripLabel tslChainNote;
+        private System.Windows.Forms.ToolStripLabel tslSlideLeftNote;
     }
 }
