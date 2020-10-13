890d6006c3e698ca1b479e3bafe3f0eeba7c3e74
TinyTany <t4nishi.x@gmail.com>
2020-10-14 02:40:20 +0900

    Add PictureBoxHandler
    追加したけどどう使おうとしていたのか忘れた

---
+++ MADCA/UI/PictureBoxHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using MADCA.Core.Data;
+
+namespace MADCA.UI
+{
+    public class PictureBoxHandler
+    {
+        private readonly PictureBox pictureBox;
+        private readonly EditorLaneEnvironment laneEnvironment;
+        private readonly PreviewDisplayEnvironment previewEnvironment;
+
+        public Size Size
+        {
+            set
+            {
+                pictureBox.Size = value;
+                pictureBox.Refresh();
+            }
+        }
+
+        private PictureBoxHandler() { }
+
+        public PictureBoxHandler(PictureBox box)
+        {
+            pictureBox = box;
+            
+        }
+    }
+}
