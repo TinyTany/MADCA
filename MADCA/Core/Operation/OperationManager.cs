8b167263c8906101902dea77599dac7c0729b57f
TinyTany <t4nishi.x@gmail.com>
2020-10-14 23:21:00 +0900

    Update OperationManager.cs
    Undo, Redoの可能不可能が切り替わるときにイベントを発火するようにした（UIへの反映に使う）

--- MADCA/Core/Operation/OperationManager.cs
+++ MADCA/Core/Operation/OperationManager.cs
@@ -10,16 +10,30 @@ namespace MADCA.Core.Operation
     {
         private readonly Stack<Operation> stackUndo, stackRedo;
 
+        public event Action<bool> UndoStatusChanged, RedoStatusChanged;
+        private bool undoStatus, redoStatus;
+
         public OperationManager()
         {
             stackUndo = new Stack<Operation>();
             stackRedo = new Stack<Operation>();
+            undoStatus = redoStatus = false;
         }
 
-        public void AddOperation(Operation op)
+        private void AddOperation(Operation op)
         {
             stackUndo.Push(op);
             stackRedo.Clear();
+            if (!undoStatus)
+            {
+                undoStatus = true;
+                UndoStatusChanged?.Invoke(undoStatus);
+            }
+            if (redoStatus)
+            {
+                redoStatus = false;
+                RedoStatusChanged?.Invoke(redoStatus);
+            }
         }
 
         public void AddAndInvokeOperation(Operation op)
@@ -35,6 +49,16 @@ namespace MADCA.Core.Operation
                 var op = stackUndo.Pop();
                 op.Undo();
                 stackRedo.Push(op);
+                if (undoStatus && !stackUndo.Any())
+                {
+                    undoStatus = false;
+                    UndoStatusChanged?.Invoke(undoStatus);
+                }
+                if (!redoStatus)
+                {
+                    redoStatus = true;
+                    RedoStatusChanged?.Invoke(redoStatus);
+                }
             }
         }
 
@@ -45,6 +69,16 @@ namespace MADCA.Core.Operation
                 var op = stackRedo.Pop();
                 op.Invoke();
                 stackUndo.Push(op);
+                if (!undoStatus)
+                {
+                    undoStatus = true;
+                    UndoStatusChanged?.Invoke(undoStatus);
+                }
+                if (redoStatus && !stackRedo.Any())
+                {
+                    redoStatus = false;
+                    RedoStatusChanged?.Invoke(redoStatus);
+                }
             }
         }
     }
