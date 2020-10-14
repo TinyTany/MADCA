3a22e4745d1b4364533df2c83d3856cf7b08a97a
TinyTany <t4nishi.x@gmail.com>
2020-10-15 01:10:36 +0900

    ボタンなどのUI部品追加

--- MADCA/UI/MainForm.Designer.cs
+++ MADCA/UI/MainForm.Designer.cs
@@ -28,16 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiQuit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.tslTouchNote = new System.Windows.Forms.ToolStripLabel();
-            this.tslChainNote = new System.Windows.Forms.ToolStripLabel();
-            this.tslSlideLeftNote = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbAdd = new System.Windows.Forms.ToolStripButton();
+            this.tsbEdit = new System.Windows.Forms.ToolStripButton();
+            this.tsbDelete = new System.Windows.Forms.ToolStripButton();
+            this.tsbRedo = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbUndo = new System.Windows.Forms.ToolStripButton();
+            this.tsbExport = new System.Windows.Forms.ToolStripButton();
+            this.tsbSave = new System.Windows.Forms.ToolStripButton();
+            this.tsbNew = new System.Windows.Forms.ToolStripButton();
+            this.tsbOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.tsbField = new System.Windows.Forms.ToolStripButton();
+            this.tsbHoldStep = new System.Windows.Forms.ToolStripButton();
+            this.tsbTouch = new System.Windows.Forms.ToolStripButton();
+            this.tsbChain = new System.Windows.Forms.ToolStripButton();
+            this.tsbSlideL = new System.Windows.Forms.ToolStripButton();
+            this.tsbSlideR = new System.Windows.Forms.ToolStripButton();
+            this.tsbSnapU = new System.Windows.Forms.ToolStripButton();
+            this.tsbSnapD = new System.Windows.Forms.ToolStripButton();
+            this.tsbHold = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.menuStrip.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -64,51 +85,239 @@
             // 
             this.tsmiOpen.Name = "tsmiOpen";
             this.tsmiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.tsmiOpen.Size = new System.Drawing.Size(161, 22);
+            this.tsmiOpen.Size = new System.Drawing.Size(180, 22);
             this.tsmiOpen.Text = "開く(&O)...";
             // 
             // tsmiQuit
             // 
             this.tsmiQuit.Name = "tsmiQuit";
             this.tsmiQuit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
-            this.tsmiQuit.Size = new System.Drawing.Size(161, 22);
+            this.tsmiQuit.Size = new System.Drawing.Size(180, 22);
             this.tsmiQuit.Text = "終了(&Q)";
             // 
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tslTouchNote,
-            this.tslChainNote,
-            this.tslSlideLeftNote});
+            this.tsbNew,
+            this.tsbOpen,
+            this.tsbSave,
+            this.tsbExport,
+            this.toolStripSeparator2,
+            this.tsbUndo,
+            this.tsbRedo,
+            this.toolStripSeparator1,
+            this.tsbAdd,
+            this.tsbEdit,
+            this.tsbDelete});
             this.toolStrip1.Location = new System.Drawing.Point(0, 24);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(1634, 25);
             this.toolStrip1.TabIndex = 1;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // tslTouchNote
+            // toolStripSeparator1
             // 
-            this.tslTouchNote.Name = "tslTouchNote";
-            this.tslTouchNote.Size = new System.Drawing.Size(39, 22);
-            this.tslTouchNote.Text = "Touch";
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
-            // tslChainNote
+            // tsbAdd
             // 
-            this.tslChainNote.Name = "tslChainNote";
-            this.tslChainNote.Size = new System.Drawing.Size(37, 22);
-            this.tslChainNote.Text = "Chain";
+            this.tsbAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbAdd.Image = ((System.Drawing.Image)(resources.GetObject("tsbAdd.Image")));
+            this.tsbAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbAdd.Name = "tsbAdd";
+            this.tsbAdd.Size = new System.Drawing.Size(33, 22);
+            this.tsbAdd.Text = "Add";
             // 
-            // tslSlideLeftNote
+            // tsbEdit
             // 
-            this.tslSlideLeftNote.Name = "tslSlideLeftNote";
-            this.tslSlideLeftNote.Size = new System.Drawing.Size(52, 22);
-            this.tslSlideLeftNote.Text = "SlideLeft";
+            this.tsbEdit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbEdit.Image = ((System.Drawing.Image)(resources.GetObject("tsbEdit.Image")));
+            this.tsbEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEdit.Name = "tsbEdit";
+            this.tsbEdit.Size = new System.Drawing.Size(31, 22);
+            this.tsbEdit.Text = "Edit";
+            // 
+            // tsbDelete
+            // 
+            this.tsbDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbDelete.Image = ((System.Drawing.Image)(resources.GetObject("tsbDelete.Image")));
+            this.tsbDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbDelete.Name = "tsbDelete";
+            this.tsbDelete.Size = new System.Drawing.Size(44, 22);
+            this.tsbDelete.Text = "Delete";
+            // 
+            // tsbRedo
+            // 
+            this.tsbRedo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbRedo.Image = ((System.Drawing.Image)(resources.GetObject("tsbRedo.Image")));
+            this.tsbRedo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRedo.Name = "tsbRedo";
+            this.tsbRedo.Size = new System.Drawing.Size(38, 22);
+            this.tsbRedo.Text = "Redo";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbUndo
+            // 
+            this.tsbUndo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbUndo.Image = ((System.Drawing.Image)(resources.GetObject("tsbUndo.Image")));
+            this.tsbUndo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbUndo.Name = "tsbUndo";
+            this.tsbUndo.Size = new System.Drawing.Size(40, 22);
+            this.tsbUndo.Text = "Undo";
+            // 
+            // tsbExport
+            // 
+            this.tsbExport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbExport.Image = ((System.Drawing.Image)(resources.GetObject("tsbExport.Image")));
+            this.tsbExport.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExport.Name = "tsbExport";
+            this.tsbExport.Size = new System.Drawing.Size(45, 22);
+            this.tsbExport.Text = "Export";
+            // 
+            // tsbSave
+            // 
+            this.tsbSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSave.Image = ((System.Drawing.Image)(resources.GetObject("tsbSave.Image")));
+            this.tsbSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSave.Name = "tsbSave";
+            this.tsbSave.Size = new System.Drawing.Size(35, 22);
+            this.tsbSave.Text = "Save";
+            // 
+            // tsbNew
+            // 
+            this.tsbNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbNew.Image = ((System.Drawing.Image)(resources.GetObject("tsbNew.Image")));
+            this.tsbNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNew.Name = "tsbNew";
+            this.tsbNew.Size = new System.Drawing.Size(35, 22);
+            this.tsbNew.Text = "New";
+            // 
+            // tsbOpen
+            // 
+            this.tsbOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbOpen.Image = ((System.Drawing.Image)(resources.GetObject("tsbOpen.Image")));
+            this.tsbOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbOpen.Name = "tsbOpen";
+            this.tsbOpen.Size = new System.Drawing.Size(40, 22);
+            this.tsbOpen.Text = "Open";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbTouch,
+            this.tsbChain,
+            this.tsbSlideL,
+            this.tsbSlideR,
+            this.tsbSnapU,
+            this.tsbSnapD,
+            this.tsbHold,
+            this.tsbHoldStep,
+            this.tsbField,
+            this.toolStripLabel1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 49);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(1634, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // tsbField
+            // 
+            this.tsbField.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbField.Image = ((System.Drawing.Image)(resources.GetObject("tsbField.Image")));
+            this.tsbField.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbField.Name = "tsbField";
+            this.tsbField.Size = new System.Drawing.Size(36, 22);
+            this.tsbField.Text = "Field";
+            // 
+            // tsbHoldStep
+            // 
+            this.tsbHoldStep.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbHoldStep.Image = ((System.Drawing.Image)(resources.GetObject("tsbHoldStep.Image")));
+            this.tsbHoldStep.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbHoldStep.Name = "tsbHoldStep";
+            this.tsbHoldStep.Size = new System.Drawing.Size(60, 22);
+            this.tsbHoldStep.Text = "HoldStep";
+            // 
+            // tsbTouch
+            // 
+            this.tsbTouch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbTouch.Image = ((System.Drawing.Image)(resources.GetObject("tsbTouch.Image")));
+            this.tsbTouch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbTouch.Name = "tsbTouch";
+            this.tsbTouch.Size = new System.Drawing.Size(43, 22);
+            this.tsbTouch.Text = "Touch";
+            // 
+            // tsbChain
+            // 
+            this.tsbChain.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbChain.Image = ((System.Drawing.Image)(resources.GetObject("tsbChain.Image")));
+            this.tsbChain.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbChain.Name = "tsbChain";
+            this.tsbChain.Size = new System.Drawing.Size(41, 22);
+            this.tsbChain.Text = "Chain";
+            // 
+            // tsbSlideL
+            // 
+            this.tsbSlideL.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSlideL.Image = ((System.Drawing.Image)(resources.GetObject("tsbSlideL.Image")));
+            this.tsbSlideL.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSlideL.Name = "tsbSlideL";
+            this.tsbSlideL.Size = new System.Drawing.Size(42, 22);
+            this.tsbSlideL.Text = "SlideL";
+            // 
+            // tsbSlideR
+            // 
+            this.tsbSlideR.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSlideR.Image = ((System.Drawing.Image)(resources.GetObject("tsbSlideR.Image")));
+            this.tsbSlideR.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSlideR.Name = "tsbSlideR";
+            this.tsbSlideR.Size = new System.Drawing.Size(43, 22);
+            this.tsbSlideR.Text = "SlideR";
+            // 
+            // tsbSnapU
+            // 
+            this.tsbSnapU.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSnapU.Image = ((System.Drawing.Image)(resources.GetObject("tsbSnapU.Image")));
+            this.tsbSnapU.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSnapU.Name = "tsbSnapU";
+            this.tsbSnapU.Size = new System.Drawing.Size(45, 22);
+            this.tsbSnapU.Text = "SnapU";
+            // 
+            // tsbSnapD
+            // 
+            this.tsbSnapD.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSnapD.Image = ((System.Drawing.Image)(resources.GetObject("tsbSnapD.Image")));
+            this.tsbSnapD.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSnapD.Name = "tsbSnapD";
+            this.tsbSnapD.Size = new System.Drawing.Size(45, 22);
+            this.tsbSnapD.Text = "SnapD";
+            // 
+            // tsbHold
+            // 
+            this.tsbHold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbHold.Image = ((System.Drawing.Image)(resources.GetObject("tsbHold.Image")));
+            this.tsbHold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbHold.Name = "tsbHold";
+            this.tsbHold.Size = new System.Drawing.Size(37, 22);
+            this.tsbHold.Text = "Hold";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1634, 861);
+            this.Controls.Add(this.toolStrip2);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
@@ -117,6 +326,8 @@
             this.menuStrip.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,8 +340,27 @@
         private System.Windows.Forms.ToolStripMenuItem tsmiQuit;
         private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
         private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel tslTouchNote;
-        private System.Windows.Forms.ToolStripLabel tslChainNote;
-        private System.Windows.Forms.ToolStripLabel tslSlideLeftNote;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsbAdd;
+        private System.Windows.Forms.ToolStripButton tsbEdit;
+        private System.Windows.Forms.ToolStripButton tsbDelete;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsbUndo;
+        private System.Windows.Forms.ToolStripButton tsbRedo;
+        private System.Windows.Forms.ToolStripButton tsbNew;
+        private System.Windows.Forms.ToolStripButton tsbOpen;
+        private System.Windows.Forms.ToolStripButton tsbSave;
+        private System.Windows.Forms.ToolStripButton tsbExport;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton tsbField;
+        private System.Windows.Forms.ToolStripButton tsbTouch;
+        private System.Windows.Forms.ToolStripButton tsbChain;
+        private System.Windows.Forms.ToolStripButton tsbSlideL;
+        private System.Windows.Forms.ToolStripButton tsbSlideR;
+        private System.Windows.Forms.ToolStripButton tsbSnapU;
+        private System.Windows.Forms.ToolStripButton tsbSnapD;
+        private System.Windows.Forms.ToolStripButton tsbHold;
+        private System.Windows.Forms.ToolStripButton tsbHoldStep;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
     }
 }
