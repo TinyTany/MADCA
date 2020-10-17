ec6eb852183a819c04fc5b3777b4077b0df1899a
TinyTany <t4nishi.x@gmail.com>
2020-10-17 18:47:31 +0900

    Update Note.cs
    当たり判定とか計算するようの実装

--- MADCA/Core/Note/Abstract/Note.cs
+++ MADCA/Core/Note/Abstract/Note.cs
@@ -5,10 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using MADCA.Core.Data;
 using MADCA.Core.Note.Interface;
+using System.Drawing;
+using MADCA.Utility;
 
 namespace MADCA.Core.Note.Abstract
 {
-    public abstract class Note : INote
+    public abstract class NoteBase : INote
     {
         public virtual NoteType NoteType => NoteType.Unknown;
 
@@ -18,9 +20,17 @@ namespace MADCA.Core.Note.Abstract
 
         public NoteSize NoteSize { get; private set; }
 
-        protected Note() { }
+        /// <summary>
+        /// 物理的当たり判定（画面上の絶対実座標位置と大きさ）
+        /// </summary>
+        public Rectangle Region { get; private set; }
 
-        protected Note(LanePotision lane, TimingPosition timing, NoteSize size)
+        // TODO: ノーツの位置や大きさが変わったときにRegionにそれが反映されるようにする
+        // NOTE: GetRectangleメソッドに全部任せてしまうのもアリ？（どのくらいパフォーマンスに違いがあるのだろうか？）
+
+        protected NoteBase() { }
+
+        protected NoteBase(LanePotision lane, TimingPosition timing, NoteSize size)
         {
             Lane = new LanePotision(lane);
             Timing = new TimingPosition(timing);
@@ -38,6 +48,14 @@ namespace MADCA.Core.Note.Abstract
         {
             NoteSize = new NoteSize(size);
             return true;
+        }
+
+        public Rectangle GetRectangle(IReadOnlyEditorLaneEnvironment env)
+        {
+            var loc = PositionConverter.ConvertVirtualToRealNorm(env, new Position(Lane, Timing));
+            int width = NoteEnvironment.NoteWidth(NoteSize.Size, env);
+            int height = NoteEnvironment.NoteHeight;
+            return new Rectangle(loc.X, loc.Y - NoteEnvironment.NoteHeight / 2, width, height);
         }
     }
 }
