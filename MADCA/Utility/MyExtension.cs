6cf3792f850e5d264e0393ea85779f4a29623019
TinyTany <t4nishi.x@gmail.com>
2020-10-18 02:48:44 +0900

    IReadOnlyListの拡張メソッド実装

---
+++ MADCA/Utility/MyExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Utility
+{
+    public static class MyExtension
+    {
+        public static T Find<T>(this IReadOnlyList<T> list, Predicate<T> p)
+            where T : class
+        {
+            foreach (var item in list)
+            {
+                if (p.Invoke(item))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public static T FindLast<T>(this IReadOnlyList<T> list, Predicate<T> p)
+            where T : class
+        {
+            foreach (var item in list.Reverse())
+            {
+                if (p.Invoke(item))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+}
