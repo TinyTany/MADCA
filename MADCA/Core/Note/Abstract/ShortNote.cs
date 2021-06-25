ab40d9b5cc4bf35ecd9b5adf4a13651c56c98069
TinyTany <t4nishi.x@gmail.com>
2021-06-26 01:43:19 +0900

    NoteModeからノーツを作る関数を移動
    UI側のコードがロジック側にあるのは良くないので

--- MADCA/Core/Note/Abstract/ShortNote.cs
+++ MADCA/Core/Note/Abstract/ShortNote.cs
@@ -47,35 +47,5 @@ namespace MADCA.Core.Note.Abstract
                     return null;
             }
         }
-
-        /// <summary>
-        /// nullを返すことがあります
-        /// </summary>
-        /// <param name="lane"></param>
-        /// <param name="timing"></param>
-        /// <param name="size"></param>
-        /// <param name="mode"></param>
-        /// <returns></returns>
-        public static ShortNote Factory(LanePotision lane, TimingPosition timing, NoteSize size, NoteMode mode)
-        {
-            switch (mode)
-            {
-                case NoteMode.Touch:
-                    return new Touch(lane, timing, size);
-                case NoteMode.Chain:
-                    return new Chain(lane, timing, size);
-                case NoteMode.SlideL:
-                    return new SlideL(lane, timing, size);
-                case NoteMode.SlideR:
-                    return new SlideR(lane, timing, size);
-                case NoteMode.SnapU:
-                    return new SnapU(lane, timing, size);
-                case NoteMode.SnapD:
-                    return new SnapD(lane, timing, size);
-                default:
-                    Debug.Assert(false);
-                    return null;
-            }
-        }
     }
 }
---
+++ MADCA/Utility/MyUtil.cs
@@ -0,0 +1,45 @@
+﻿using MADCA.Core.Data;
+using MADCA.Core.Note.Abstract;
+using MADCA.Core.Note.Concrete;
+using MADCA.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MADCA.Utility
+{
+    static class MyUtil
+    {
+        /// <summary>
+        /// nullを返すことがあります
+        /// </summary>
+        /// <param name="lane"></param>
+        /// <param name="timing"></param>
+        /// <param name="size"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        public static ShortNote NoteFactory(LanePotision lane, TimingPosition timing, NoteSize size, NoteMode mode)
+        {
+            switch (mode)
+            {
+                case NoteMode.Touch:
+                    return new Touch(lane, timing, size);
+                case NoteMode.Chain:
+                    return new Chain(lane, timing, size);
+                case NoteMode.SlideL:
+                    return new SlideL(lane, timing, size);
+                case NoteMode.SlideR:
+                    return new SlideR(lane, timing, size);
+                case NoteMode.SnapU:
+                    return new SnapU(lane, timing, size);
+                case NoteMode.SnapD:
+                    return new SnapD(lane, timing, size);
+                default:
+                    //Debug.Assert(false);
+                    return null;
+            }
+        }
+    }
+}
