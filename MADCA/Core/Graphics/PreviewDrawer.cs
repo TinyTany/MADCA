48e136f9d3bec0d3c650651837a0f3011dce22fe
TinyTany <t4nishi.x@gmail.com>
2021-06-26 21:02:07 +0900

    プレビュー画面の描画周り実装
    拍数の線や、小節番号を描画するようにした

--- MADCA/Core/Graphics/PreviewDrawer.cs
+++ MADCA/Core/Graphics/PreviewDrawer.cs
@@ -12,23 +12,40 @@ namespace MADCA.Core.Graphics
         {
             using(var backBrush = new SolidBrush(Color.Black))
             using(var gameDisplayBrush = new SolidBrush(Color.FromArgb(50, 50, 50)))
-            using(var whitePen = new Pen(Color.White))
+            using(var penMain = new Pen(Color.White))
+            using(var penSub = new Pen(Color.FromArgb(130, Color.White)))
             using (Font myFont = new Font("MS UI Gothic", 10, FontStyle.Regular))
             {
                 g.FillRectangle(backBrush,env.DisplayRegion);
                 g.FillEllipse(gameDisplayBrush, env.Circle);
-                g.DrawEllipse(whitePen, env.Circle);
+                g.DrawEllipse(penMain, env.Circle);
 
                 var circleCenter = new Point(env.Circle.X + env.Radius, env.Circle.Y + env.Radius);
                 var vanishingTiming = env.TimingOffset + env.TimingLength;
-                foreach (var score in scores.Where(x => env.TimingOffset < x.TimingBegin && x.TimingBegin < vanishingTiming))
+                foreach (var score in scores.Where(x => env.TimingOffset < x.TimingEnd && x.TimingBegin < vanishingTiming))
                 {
+                    // 主線小節番号の描画
                     var r = (int)(((vanishingTiming - score.TimingBegin) / env.TimingLength).BarRatio * env.Radius);
                     var c = new Rectangle(circleCenter.X - r, circleCenter.Y - r, 2 * r, 2 * r);
-                    g.DrawEllipse(whitePen, c);
-                    // TODO: 表示/非表示を切り替えられるようにしたら良いかもね
-                    g.DrawString(scores.IndexOf(score).ToString().PadLeft(3, '0'), myFont, Brushes.White, new Point(c.Left + c.Width / 2 - 14, c.Bottom - 6));
+                    if (0 < r && r < env.Radius)
+                    {
+                        g.DrawEllipse(penMain, c);
+                        // TODO: 表示/非表示を切り替えられるようにしたら良いかもね
+                        g.DrawString(scores.IndexOf(score).ToString().PadLeft(3, '0'), myFont, Brushes.White, new Point(c.Left + c.Width / 2 - 14, c.Bottom - 6));
+                    }
 
+                    // 副線の描画
+                    // TODO: 表示/非表示を切り替えられるようにしたら良いかもね
+                    for (var cnt = 1; cnt < score.BeatNum; ++cnt)
+                    {
+                        r = (int)(((vanishingTiming - score.TimingBegin - new TimingPosition(score.BeatDen, cnt)) / env.TimingLength).BarRatio * env.Radius);
+                        if (!(0 < r && r < env.Radius))
+                        {
+                            continue;
+                        }
+                        c = new Rectangle(circleCenter.X - r, circleCenter.Y - r, 2 * r, 2 * r);
+                        g.DrawEllipse(penSub, c);
+                    }
                 }
             }
         }
